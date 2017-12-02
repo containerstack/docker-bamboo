787cdccc83f32ea44729ada52e490e23cd56574d
Remon Lam <remon@containerstack.io>
2017-12-02 20:14:52 +0100

    update entrypoint

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,28 +1,35 @@
 #!/bin/bash
+set -euo pipefail
 
-# check if the `server.xml` file has been changed since the creation of this
-# Docker image. If the file has been changed the entrypoint script will not
-# perform modifications to the configuration file.
-if [ "$(stat --format "%Y" "${BAMBOO_INSTALL}/conf/server.xml")" -eq "0" ]; then
-  echo "doing something"
-  if [ -n "${X_PROXY_NAME}" ]; then
-    echo "setting proxy name"
-    xmlstarlet ed --inplace --pf --ps --insert '//Connector[@port="8085"]' --type "attr" --name "proxyName" --value "${X_PROXY_NAME}" "${BAMBOO_INSTALL}/conf/server.xml"
-  fi
-  if [ -n "${X_PROXY_PORT}" ]; then
-    echo "setting proxyport"
-    xmlstarlet ed --inplace --pf --ps --insert '//Connector[@port="8085"]' --type "attr" --name "proxyPort" --value "${X_PROXY_PORT}" "${BAMBOO_INSTALL}/conf/server.xml"
-  fi
-  if [ -n "${X_PROXY_SCHEME}" ]; then
-    xmlstarlet ed --inplace --pf --ps --insert '//Connector[@port="8085"]' --type "attr" --name "scheme" --value "${X_PROXY_SCHEME}" "${BAMBOO_INSTALL}/conf/server.xml"
-  fi
-  if [ "${X_PROXY_SCHEME}" = "https" ]; then
-    xmlstarlet ed --inplace --pf --ps --insert '//Connector[@port="8085"]' --type "attr" --name "secure" --value "true" "${BAMBOO_INSTALL}/conf/server.xml"
-    xmlstarlet ed --inplace --pf --ps --update '//Connector[@port="8085"]/@redirectPort' --value "${X_PROXY_PORT}" "${BAMBOO_INSTALL}/conf/server.xml"
-  fi
-  if [ -n "${X_PATH}" ]; then
-    xmlstarlet ed --inplace --pf --ps --update '//Context/@path' --value "${X_PATH}" "${BAMBOO_INSTALL}/conf/server.xml"
-  fi
+# Setup Catalina Opts
+: ${CATALINA_CONNECTOR_PROXYNAME:=}
+: ${CATALINA_CONNECTOR_PROXYPORT:=}
+: ${CATALINA_CONNECTOR_SCHEME:=http}
+: ${CATALINA_CONNECTOR_SECURE:=false}
+
+: ${CATALINA_OPTS:=}
+
+: ${JAVA_OPTS:=}
+
+CATALINA_OPTS="${CATALINA_OPTS} -DcatalinaConnectorProxyName=${CATALINA_CONNECTOR_PROXYNAME}"
+CATALINA_OPTS="${CATALINA_OPTS} -DcatalinaConnectorProxyPort=${CATALINA_CONNECTOR_PROXYPORT}"
+CATALINA_OPTS="${CATALINA_OPTS} -DcatalinaConnectorScheme=${CATALINA_CONNECTOR_SCHEME}"
+CATALINA_OPTS="${CATALINA_OPTS} -DcatalinaConnectorSecure=${CATALINA_CONNECTOR_SECURE}"
+
+export JAVA_OPTS="${JAVA_OPTS} ${CATALINA_OPTS}"
+
+
+# Start Bamboo as the correct user
+if [ "${UID}" -eq 0 ]; then
+    echo "User is currently root. Will change directory ownership to ${RUN_USER}:${RUN_GROUP}, then downgrade permission to ${RUN_USER}"
+    PERMISSIONS_SIGNATURE=$(stat -c "%u:%U:%a" "${BAMBOO_HOME}")
+    EXPECTED_PERMISSIONS=$(id -u ${RUN_USER}):${RUN_USER}:700
+    if [ "${PERMISSIONS_SIGNATURE}" != "${EXPECTED_PERMISSIONS}" ]; then
+        chmod -R 700 "${BAMBOO_HOME}" &&
+            chown -R "${RUN_USER}:${RUN_GROUP}" "${BAMBOO_HOME}"
+    fi
+    # Now drop privileges
+    exec su -s /bin/bash "${RUN_USER}" -c "$BAMBOO_INSTALL_DIR/bin/start-bamboo.sh $@"
+else
+    exec "$BAMBOO_INSTALL_DIR/bin/start-bamboo.sh" "$@"
 fi
-
-exec "$@"
